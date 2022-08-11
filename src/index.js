b60deeddc6dae212b805c580d2d22942d94497ba
Claudio Miranda <cronosunder@gmail.com>
2022-08-11 11:49:55 -0400

    message levels

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const { getInput, setFailed } = require("@actions/core");
 const PullRequestChecker = require("./pullRequestChecker");
 
 async function run() {
-	try {
-		await new PullRequestChecker(
-			getInput("repo-token", { required: true }),
-			getInput("mesaje-block", { required: true }),
-		).process();
-	} catch (error) {
-		setFailed(error.message);
-	}
+    try {
+        await new PullRequestChecker(
+            getInput("repo-token", { required: true }),
+            getInput("mesaje-block", { required: true }),
+        ).process();
+    } catch (error) {
+        setFailed(error.message);
+    }
 }
 
 run();
--- src/pullRequestChecker.js
+++ src/pullRequestChecker.js
@@ -1,41 +1,41 @@
-const { debug, error } = require("@actions/core");
+const { error, info } = require("@actions/core");
 const { context, getOctokit } = require("@actions/github");
 
-class PullRequestChecker {
-	constructor(repoToken, mesajeBlock) {
-		this.client = getOctokit(repoToken);
-		this.mesajeBlock = mesajeBlock;
-	}
+class PullRequestChecker{
+    constructor(repoToken, mesajeBlock) {
+        this.client = getOctokit(repoToken);
+        this.mesajeBlock = mesajeBlock;
+    }
 
-	async process() {
-		const commits = await this.client.paginate(
-			"GET /repos/{owner}/{repo}/pulls/{pull_number}/commits",
-			{
-				...context.repo,
-				pull_number: context.issue.number,
-				per_page: 100,
-			},
-		);
-		const branch = process.env.GITHUB_REF.split("/").slice(2).join("/");
-		debug(`${commits.length} commit(s) in the pull request branch ${branch}`);
-		const mensaje = this.mesajeBlock.replace("{actual-branch}", branch);
-		let blockedCommits = 0;
-		for (const {
-			commit: { message },
-			sha,
-			url,
-		} of commits) {
-			const isAutosquash = message.includes(mensaje);
-
-			if (isAutosquash) {
-				error(`Commit ${sha} is an blocked commit: ${url}`);
-				blockedCommits++;
-			}
-		}
-		if (blockedCommits) {
-			throw Error(`${blockedCommits} commit(s) need to be squashed`);
-		}
-	}
+    async process() {
+        const commits = await this.client.paginate(
+            "GET /repos/{owner}/{repo}/pulls/{pull_number}/commits",
+            {
+                ...context.repo,
+                pull_number: context.issue.number,
+                per_page: 100
+            }
+        );
+        const branch = process.env.GITHUB_REF.split("/").slice(2).join("/");
+        info(`${commits.length} commit(s) in the pull request branch ${branch}`);
+        const mensaje = this.mesajeBlock.replace("{actual-branch}", branch);
+        let blockedCommits = 0;
+        for (const {
+            commit: { message },
+            sha,
+            url,
+        } of commits) {
+            const constainMessage = message.includes(mensaje);
+            info(`${sha} ${message} ${constainMessage} ${mensaje}`);
+            if (constainMessage) {
+                error(`Commit ${sha} is an blocked commit: ${url}`);
+                blockedCommits++;
+            }
+        }
+        if (blockedCommits) {
+            throw Error(`${blockedCommits} commit(s) need to be squashed`);
+        }
+    }
 }
 
 module.exports = PullRequestChecker;
