fa0e83c3726df5d5a4412bce212b2095382a87eb
Claudio Miranda <cronosunder@gmail.com>
2022-08-11 13:43:08 -0400

    build

--- dist/index.js
+++ dist/index.js
@@ -9487,113 +9487,6 @@ function wrappy (fn, cb) {
 
 /***/ }),
 
-/***/ 109:
-/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
-
-const core = __nccwpck_require__(2186);
-
-const { run } = __nccwpck_require__(9575);
-
-/** @typedef {import('./github/context').GithubContext} GithubContext */
-
-/**
- * Fetches and checks out the remote Git branch (if it exists, the fork repository will be used)
- * @param {GithubContext} context - Information about the GitHub
- */
-function checkOutRemoteBranch(context) {
-    if (context.repository.hasFork) {
-        // Fork: Add fork repo as remote
-        core.info(`Adding "${context.repository.forkName}" fork as remote with Git`);
-        const cloneURl = new URL(context.repository.forkCloneUrl);
-        cloneURl.username = context.actor;
-        cloneURl.password = context.token;
-        run(`git remote add fork ${cloneURl.toString()}`);
-    } else {
-        // No fork: Update remote URL to include auth information (so auto-fixes can be pushed)
-        core.info("Adding auth information to Git remote URL");
-        const cloneURl = new URL(context.repository.cloneUrl);
-        cloneURl.username = context.actor;
-        cloneURl.password = context.token;
-        run(`git remote set-url origin ${cloneURl.toString()}`);
-    }
-
-    const remote = context.repository.hasFork ? "fork" : "origin";
-
-    // Fetch remote branch
-    core.info(`Fetching remote branch "${context.branch}"`);
-    run(`git fetch --no-tags --depth=1 ${remote} ${context.branch}`);
-
-    // Switch to remote branch
-    core.info(`Switching to the "${context.branch}" branch`);
-    run(`git branch --force ${context.branch} --track ${remote}/${context.branch}`);
-    run(`git checkout ${context.branch}`);
-}
-
-/**
- * Stages and commits all changes using Git
- * @param {string} message - Git commit message
- * @param {boolean} skipVerification - Skip Git verification
- */
-function commitChanges(message, skipVerification) {
-    core.info("Committing changes");
-    run(`git commit -am "${message}"${skipVerification ? " --no-verify" : ""}`);
-}
-
-/**
- * Returns the SHA of the head commit
- * @returns {string} - Head SHA
- */
-function getHeadSha() {
-    const sha = run("git rev-parse HEAD").stdout;
-    core.info(`SHA of last commit is "${sha}"`);
-
-    return sha;
-}
-
-/**
- * Checks whether there are differences from HEAD
- * @returns {boolean} - Boolean indicating whether changes exist
- */
-function hasChanges() {
-    const output = run("git diff-index --name-status --exit-code HEAD --", { ignoreErrors: true });
-    const hasChangedFiles = output.status === 1;
-    core.info(`${hasChangedFiles ? "Changes" : "No changes"} found with Git`);
-
-    return hasChangedFiles;
-}
-
-/**
- * Pushes all changes to the remote repository
- * @param {boolean} skipVerification - Skip Git verification
- */
-function pushChanges(skipVerification) {
-    core.info("Pushing changes with Git");
-    run(`git push${skipVerification ? " --no-verify" : ""}`);
-}
-
-/**
- * Updates the global Git configuration with the provided information
- * @param {string} name - Git username
- * @param {string} email - Git email address
- */
-function setUserInfo(name, email) {
-    core.info("Setting Git user information");
-    run(`git config --global user.name "${name}"`);
-    run(`git config --global user.email "${email}"`);
-}
-
-module.exports = {
-    checkOutRemoteBranch,
-    commitChanges,
-    getHeadSha,
-    hasChanges,
-    pushChanges,
-    setUserInfo,
-};
-
-
-/***/ }),
-
 /***/ 6476:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
@@ -10024,18 +9917,17 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const core = __nccwpck_require__(2186);
-const git = __nccwpck_require__(109);
-
 const { getContext } = __nccwpck_require__(6476);
-const { getOctokit } = __nccwpck_require__(5438);
+const { context, getOctokit } = __nccwpck_require__(5438);
 const github = __nccwpck_require__(5438);
 const { info, error } = __nccwpck_require__(2186);
 
 async function runAction() {
-	const context = getContext();
+	const pull_request_number = context.issue.number;
+	const repo = context.repo;
 	const repoToken = core.getInput("github_token", { required: true });
 	const messaje = core.getInput("messaje", { required: true });
-	const pull_request_number = context.payload.pull_request.number;
+	const envContext = getContext();
 
 	core.startGroup(`Run action : ${pull_request_number}` + messaje);
 	const client = getOctokit(repoToken);
@@ -10043,7 +9935,7 @@ async function runAction() {
 	const commits = await client.paginate(
 		"GET /repos/{owner}/{repo}/pulls/{pull_number}/commits",
 		{
-			...context.repo,
+			...repo,
 			pull_number: pull_request_number,
 			per_page: 100
 		}
@@ -10066,7 +9958,7 @@ async function runAction() {
 			error(`Commit ${sha} is an blocked commit: ${url}`);
 			blockedCommits += 1;
 			await octokit.issues.createComment({
-				...context.repo,
+				...envContext.repo,
 				issue_number: pull_request_number,
 				body: `Commit ${sha} is an blocked commit: ${mensaje}`
 			});
